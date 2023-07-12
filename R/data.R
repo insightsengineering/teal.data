a5ec57ca06ae170afe661b244b96cc1a9537ab53
kartikeya <kartikeya.kirar@unicle.life>
2023-07-12 09:20:45 +0530

    Added dataset for testing examples and removed scda from description.

---
+++ R/data.R
@@ -0,0 +1,103 @@
+#' Random adverse events
+#'
+#' @description Random adverse events
+#' @docType data
+#'
+#' @usage rADAE
+#'
+#' @keywords datasets internal
+#'
+#' @source internal
+#' @name rADAE
+"rADAE"
+
+#' Random Concomitant Medications
+#'
+#' @description Random Concomitant Medications
+#' @docType data
+#'
+#' @usage rADCM
+#'
+#' @keywords datasets internal
+#'
+#' @source internal
+#' @name rADCM
+"rADCM"
+
+#' Random response
+#'
+#' @description Random Exposure
+#' @docType data
+#'
+#' @usage rADEX
+#'
+#' @keywords datasets internal
+#'
+#' @source internal
+#' @name rADEX
+"rADEX"
+
+#' Random lab analysis
+#'
+#' @description Random lab analysis
+#' @docType data
+#'
+#' @usage rADLB
+#'
+#' @keywords datasets internal
+#'
+#' @source internal
+#' @name rADLB
+"rADLB"
+
+#' Random response
+#'
+#' @description Random response
+#' @docType data
+#'
+#' @usage rADRS
+#'
+#' @keywords datasets internal
+#'
+#' @source internal
+#' @name rADRS
+"rADRS"
+
+#' Random patient listing
+#'
+#' @description Random patient listing
+#' @docType data
+#'
+#' @usage rADSL
+#'
+#' @keywords datasets internal
+#'
+#' @source internal
+#' @name rADSL
+"rADSL"
+
+#' Random data `rADTR`
+#'
+#' @description Random data `rADTR`
+#' @docType data
+#'
+#' @usage rADTR
+#'
+#' @keywords datasets internal
+#'
+#' @source internal
+#' @name rADTR
+"rADTR"
+
+#' Random Time to Event Analysis Dataset
+#'
+#' @description Random Time to Event Analysis Dataset
+#' @docType data
+#'
+#' @usage rADTTE
+#'
+#' @keywords datasets internal
+#'
+#' @source internal
+#' @name rADTTE
+"rADTTE"
---
+++ data-raw/data.R
@@ -0,0 +1,25 @@
+## code to prepare `data` for testing examples
+library(scda)
+rADAE <- synthetic_cdisc_data("latest")$adae # nolint
+usethis::use_data(rADAE)
+
+rADCM <- synthetic_cdisc_data("latest")$adcm # nolint
+usethis::use_data(rADCM)
+
+rADEX <- synthetic_cdisc_data("latest")$adex # nolint
+usethis::use_data(rADEX)
+
+rADLB <- synthetic_cdisc_data("latest")$adlb # nolint
+usethis::use_data(rADLB)
+
+rADRS <- synthetic_cdisc_data("latest")$adrs # nolint
+usethis::use_data(rADRS)
+
+rADSL <- synthetic_cdisc_data("latest")$adsl # nolint
+usethis::use_data(rADSL)
+
+rADTR <- synthetic_cdisc_data("latest")$adtr # nolint
+usethis::use_data(rADTR)
+
+rADTTE <- synthetic_cdisc_data("latest")$adtte # nolint
+usethis::use_data(rADTTE)
