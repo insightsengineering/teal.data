2355695572af739b1ff3c9a8db47706ebbccd5e8
go_gonzo <dawid.kaledkowski@gmail.com>
2023-10-25 14:23:29 +0200

    move get_join_keys.default from teal

--- R/get_join_keys.R
+++ R/get_join_keys.R
@@ -8,6 +8,12 @@ get_join_keys <- function(data) {
 
 #' @rdname get_join_keys
 #' @export
+get_join_keys.default <- function(data) {
+  stop("get_join_keys function not implemented for object of class ", toString(class(data)))
+}
+
+#' @rdname get_join_keys
+#' @export
 get_join_keys.teal_data <- function(data) {
   data@join_keys
 }
@@ -25,6 +31,7 @@ get_join_keys.JoinKeys <- function(data) {
 `get_join_keys<-` <- function(data, dataset_1, dataset_2 = NULL, value) {
   UseMethod("get_join_keys<-", data)
 }
+
 
 #' @rdname get_join_keys
 #' @inheritParams mutate_join_keys
