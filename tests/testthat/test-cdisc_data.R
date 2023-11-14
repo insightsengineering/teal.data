1efb3ad065c702714bcec0589001813d3ed0db9b
André Veríssimo <211358+averissimo@users.noreply.github.com>
2023-11-14 01:50:40 +0100

    cleanup: move cdisc helper fn

--- tests/testthat/test-cdisc_data.R
+++ tests/testthat/test-cdisc_data.R
@@ -1,39 +1,5 @@
-cdisc_data_mixed_call <- function(check = TRUE, join_keys1 = cdisc_join_keys()) {
-  adsl_raw <- as.data.frame(as.list(setNames(nm = get_cdisc_keys("ADSL"))))
-  adtte_raw <- as.data.frame(as.list(setNames(nm = get_cdisc_keys("ADTTE"))))
-  adae_raw <- as.data.frame(as.list(setNames(nm = get_cdisc_keys("ADAE"))))
-
-  adsl <- cdisc_dataset(
-    dataname = "ADSL",
-    x = adsl_raw,
-    code = "ADSL <- as.data.frame(as.list(setNames(nm = get_cdisc_keys(\"ADSL\"))))"
-  )
-  adtte_cf <- callable_function(
-    function() {
-      as.data.frame(as.list(setNames(nm = get_cdisc_keys("ADTTE"))))
-    }
-  )
-  adtte <- cdisc_dataset_connector("ADTTE", adtte_cf, keys = get_cdisc_keys("ADTTE"), vars = list(x = adsl))
-  adae_cf <- callable_function(
-    function() {
-      as.data.frame(as.list(setNames(nm = get_cdisc_keys("ADAE"))))
-    }
-  )
-  adae_cdc <- cdisc_dataset_connector("ADAE", adae_cf, keys = get_cdisc_keys("ADAE"))
-  adae_rdc <- cdisc_data_connector(
-    connection = data_connection(open_fun = callable_function(function() "open function")),
-    connectors = list(adae_cdc)
-  )
-
-  load_dataset(adsl)
-  load_dataset(adtte)
-  load_dataset(adae_cdc)
-
-  cdisc_data(adsl, adtte, adae_rdc, check = check, join_keys = join_keys1)
-}
-
 testthat::test_that("cdisc_data accepts TealDataset, TealDatasetConnector, TealDataConnector objects", {
-  lifecycle::expect_deprecated(data <- cdisc_data_mixed_call(), "should use data directly")
+  lifecycle::expect_deprecated(data <- local_cdisc_data_mixed_call(), "should use data directly")
   testthat::expect_identical(data$get_datanames(), c("ADSL", "ADTTE", "ADAE"))
 })
 
@@ -46,7 +12,7 @@ testthat::test_that("cdisc_data returns teal_data object for objects different t
 })
 
 testthat::test_that("cdisc_data sets the join_keys internally", {
-  data <- cdisc_data_mixed_call()
+  data <- local_cdisc_data_mixed_call()
 
   jks <- join_keys(
     join_key("ADSL", "ADSL", c("STUDYID", "USUBJID")),
@@ -64,7 +30,7 @@ testthat::test_that(
   "cdisc_data sets the join_keys internally based on parents relations when primary keys are altered",
   {
     jks <- join_keys(join_key("ADSL", "ADSL", c("STUDYID")))
-    data <- cdisc_data_mixed_call(join_keys1 = jks)
+    data <- local_cdisc_data_mixed_call(join_keys1 = jks)
 
     jks <- join_keys(
       join_key("ADSL", "ADSL", c("STUDYID")),
