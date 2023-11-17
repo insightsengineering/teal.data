935aae4772f18fdc59a0a545399c4bdacd204699
André Veríssimo <211358+averissimo@users.noreply.github.com>
2023-11-17 18:25:56 +0100

    feat: move compare function to exported all.equal

---
+++ R/join_keys-all.equal.R
@@ -0,0 +1,50 @@
+#' Test if Two Objects are (Nearly) Equal
+#'
+#' `all.equal(target, current)` is a utility to compare `join_keys` objects target
+#' and current testing ‘near equality’.
+#'
+#' If they are different, comparison is still made to some extent, and a report
+#' of the differences is returned.
+#' Do not use all.equal directly in if expressions—either use isTRUE(all.equal(....))
+#' or identical if appropriate.
+#'
+#' @inheritParams base::all.equal
+#' @param ... further arguments for different methods. Not used with `join_keys`.
+#'
+#' @details
+#' The parents attribute comparison tolerates `NULL` and empty lists and will find
+#' no difference.
+#'
+#' The list containing all the relationships is treated like a map and ignores
+#' entries with `NULL` if they exist.
+#'
+#' @export
+all.equal.join_keys <- function(target, current, ...) {
+  .as_map <- function(.x) {
+    old_attributes <- attributes(.x)
+    # Keep only non-list attributes
+    old_attributes[["names"]] <- NULL
+    old_attributes[["class"]] <- NULL
+    old_attributes[["__parents__"]] <- old_attributes[["__parents__"]] %||% list()
+
+    attr(.x, "class") <- "list"
+
+    # Remove nulls
+    is_null <- vapply(.x, is.null, logical(1))
+    .x <- .x[!is_null]
+
+    # Sort named components, preserving positions of unnamed
+    nx <- rlang::names2(.x)
+    is_named <- nx != ""
+    if (any(is_named)) {
+      idx <- seq_along(.x)
+      idx[is_named] <- idx[is_named][order(nx[is_named])]
+      .x <- .x[idx]
+    }
+    attributes(.x) <- utils::modifyList(old_attributes, attributes(.x) %||% list())
+    .x
+  }
+  x <- .as_map(target)
+  y <- .as_map(current)
+  all.equal(x, y)
+}
