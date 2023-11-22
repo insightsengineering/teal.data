7d3f46148b7306a521179a79342a67c3b0ea7d0f
m7pr <marcin.kosinski.mk1@roche.com>
2023-11-22 10:58:04 +0100

    use get_code generic from teal.code

--- R/teal_data-get_code.R
+++ R/teal_data-get_code.R
@@ -4,7 +4,7 @@
 #' of `names` parameter. If input `teal_data@verified` is `FALSE` the returned code is pre-pended with a warning
 #' stating that the object was not verified.
 #'
-#' @param x [`teal_data`] object.
+#' @param object `teal_data`
 #' @param names (`character`) vector of object names to return the code for.
 #' @param deparse (`logical`) whether return deparsed form of a call.
 #' @return (`character`) code of import and preparation of data for `teal` application.
@@ -18,27 +18,26 @@
 #' })
 #' get_code(tdata1)
 #' datanames(tdata1) <- c("a", "b", "c")
-#' get_code(tdata1, "a")
-#' get_code(tdata1, "b")
+#' get_code(tdata1, names = "a")
+#' get_code(tdata1, names = "b")
 #'
 #' tdata2 <- teal_data(x1 = iris, code = "x1 = iris")
 #' get_code(tdata2)
 #' get_code(verify(tdata2))
 #' @aliases get_code,teal_data-method
 #' @export
-setGeneric("get_code", function(x, ...) standardGeneric("get_code"))
-setMethod("get_code", "teal_data", definition = function(x, names = NULL, deparse = TRUE) {
+setMethod("get_code", "teal_data", definition = function(object, deparse = TRUE, names = NULL) {
   checkmate::assert_character(names, min.len = 1L, null.ok = TRUE)
-  checkmate::assert_subset(names, x@datanames, empty.ok = TRUE)
+  checkmate::assert_subset(names, object@datanames, empty.ok = TRUE)
   checkmate::assert_flag(deparse)
 
   code <- if (!is.null(names)) {
-    get_code_dependency(x@code, names)
+    get_code_dependency(object@code, names)
   } else {
-    x@code
+    object@code
   }
 
-  if (!x@verified) {
+  if (!object@verified) {
     code <- c("warning('Code was not verified for reproducibility.')", code)
   }
 
