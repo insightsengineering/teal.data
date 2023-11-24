5c19b7bae53525f783dc6658052eeae591743612
m7pr <marcin.kosinski.mk1@roche.com>
2023-11-24 15:02:33 +0100

    curate code_parser

--- R/utils-code_dependency-2.R
+++ R/utils-code_dependency-2.R
@@ -2,19 +2,11 @@
 
 code_graph <- function(calls_pd) {
 
-  # TODO
-  # based on
-  # R/utils-code_dependency.R detect_symbol
-  # R/utils-code_dependency.R used_in_function
-  # R/utils-code_dependency.R code_dependency()->cooccurrence
-
-  # list, length = length(calls_pd)
+  # below are 3 lists, length = length(calls_pd)
   cooccurence <- extract_occurence(calls_pd)
 
-  # list, length = length(calls_pd)
   side_effects <- extract_side_effects(calls_pd)
 
-  # list, length = length(calls_pd)
   final_list <- append_side_effects(cooccurence, side_effects)
 
   # structure - character(n)
@@ -22,7 +14,7 @@ code_graph <- function(calls_pd) {
   #           - (2) character(n) # occurence AND side_effectS
   #           - (3) character(n) # occurence  OR side_effectS  and influencers
   #           - (5) character(n) # occurence AND side_effectS  and influencers
-  #           - (6) character(n) #                            only influencers
+  #           - (6) character(n) #                            only influencers  - is this possible?
   #
   #           - occurence can be character(n), e.g. "a <- b <- 5", "a <- iris"                # a, b / a
   #           - side_effectS  is character(n), e.g. "@linktso ADSL", "@linktso ADSL ADLB"     # ADSL / ADSL ADLB
@@ -31,51 +23,71 @@ code_graph <- function(calls_pd) {
   # we need a separator to differentiate between influencers
   # proposition:
   # character(n) containing ":" element that divides occurence and side_effects from influencers
-  # for case with only influencers then it is c(":", "influencer_name")
-
+  # for case with only influencers then it is c(":", "influencer_name") - is this possible?
+  final_list
 }
 
 extract_occurence <- function(calls_pd) {
-  # TO BE FINISHED:
-  # USED detect_symbol() and used_in_function() to simplify?
+
+  # TO BE POLISHED:
+  # CAN
+  # R/utils-code_dependency.R used_in_function
+  # R/utils-code_dependency.R code_dependency()
+  # HELP?
+
   lapply(
     calls_pd,
     function(x) {
-      sym_cond <- which(x$token %in% c("SYMBOL", "SYMBOL_FUNCTION_CALL") & x$text %in% names)
-      sym_form_cond <- which(x$token == "SYMBOL_FORMALS" & x$text %in% names)
+      # CAN THIS BE SIMPLIFIED
+      # x %in% "SYMBOL", "SYMBOL_FUNCTION_CALL"
+      # and x not %in% "SYMBOL_FORMALS"
+      sym_cond <- which(x$token %in% c("SYMBOL", "SYMBOL_FUNCTION_CALL"))# & x$text %in% names)
+      sym_form_cond <- which(x$token == "SYMBOL_FORMALS")# & x$text %in% names)
       sym_cond <- sym_cond[!x[sym_cond, "text"] %in% x[sym_form_cond, "text"]]
 
+      # watch out for SYMBOLS after $ and @, e.g. x$a x@a // x is object, a is not
+      # and remove those
       object_ids <- x[sym_cond, "id"]
-      dollar_ids <- x[x$"token" %in% c("'$'", "'@'"), "id"]
+      dollar_ids <- x[x$token %in% c("'$'", "'@'"), "id"]
       after_dollar <- object_ids[(object_ids - 2) %in% dollar_ids]
       sym_cond <- setdiff(sym_cond, which(x$id %in% after_dollar))
 
+      # sym_cond is row idenfitier in x
+      # x is an element of calls_pd
+
+      # if there was an assignment operation
       if (length(sym_cond) >= 2) {
         ass_cond <- grep("ASSIGN", x$token)
-        text <- unique(x[sort(c(sym_cond, ass_cond)), "text"])
+        text <- x[sort(c(sym_cond, ass_cond)), "text"]
 
-        if (text[1] == "->") {
+        ans <- if (text[1] == "->") { # NOTE(3)
           rev(text[-1])
         } else {
           text[-1]
         }
+        c(ans[1], ":", unique(ans[-1]))
+
+      } else {
+        x[sym_cond, "text"]
       }
+      ##### NOTE(3): WHAT IS THERE ARE 2+ ASSIGNEMNTS, e.g. a <- b <- c
+      ##### NOTE(3): WHAT IS THERE ARE 2+ ASSIGNEMNTS, e.g. a <- b -> c
     }
   )
 }
 
 extract_side_effects <- function(calls_pd) {
-  # TO BE FINISHED:
-  # can this work on calls_pd
-  pd <- do.call(calls_pd, rbind) ## ??
-  side_effects <- grep("@linksto", pd[pd$token == "COMMENT", "text"], value = TRUE)
-  if (length(side_effects) > 0) {
-    affected <- # maybe this should be adjusted as well
-      unlist(strsplit(sub("\\s*#\\s*@linksto\\s+", "", side_effects), "\\s+"))
-  } else {
-    list() #?
-  }
-  # affected as a list
+  lapply(
+    calls_pd,
+    function(x) {
+      linksto <- grep("@linksto", x[x$token == "COMMENT", "text"], value = TRUE)
+      unlist(strsplit(sub("\\s*#\\s*@linksto\\s+", "", linksto), "\\s+"))
+    }
+  )
+}
+
+append_side_effects <- function(cooccurence, side_effects) {
+  mapply(c, side_effects, cooccurence, SIMPLIFY = FALSE)
 }
 
 
@@ -125,18 +137,26 @@ graph_parser <- function(x, graph, skip = NULL) {
 
   # returns: character
   influencers <- #
-    lapply(graph[which(occurence)], function(call) {
-      strsplit(call, split = ":", fixed = TRUE)[[1]][2]
-    })
+    unlist(
+      lapply(graph[occurence], function(call) {
+        call[which(":" == call)+1]
+        #strsplit(call, split = ":", fixed = TRUE)[[1]][2]
+      })
+    )
   influencers <- setdiff(influencers, skip)
 
-  # returns: logical
-  influencers_deps <-
-    lapply(influencers, function(influencer){
-      graph_parser(influencers, graph[1:max(which(occurence))], skip) ##### NOTE (2) BELOW
-    })
+  # STOPPED HERE: CONTINUE FROM HERE - almost there!
+  if (length(influencers)) {
+    # returns: logical
+    influencers_deps <-
+      lapply(influencers, function(influencer){
+        graph_parser(influencers, graph[1:max(which(occurence))], skip) ##### NOTE (2) BELOW
+      })
 
-  unique(which(occurence), which(influencers_deps))
+    unique(which(occurence), which(influencers_deps))
+  } else {
+    which(occurence)
+  }
 
   ##### NOTE (1)
   # DOUBLE CHECK: Does it also return side effects created after the object was used?
@@ -183,7 +203,8 @@ is_empty <- function(code){
   identical(code, character(0)) || identical(trimws(code), "")
 }
 
-# example code
+
+# example ---------------------------------------------------------------------------------------------------------
 
 code <- "
 
@@ -194,10 +215,40 @@ code <- "
 
 "
 
-graph <- list(
+graph_expected <- list(
   c("a"),
   c("b", ":", "a"),
-  c("a"),              # or c("a", ":", "a")
-  c("c", "a", ":", "5")
+  c("a", ":", "a"),
+  c("a", "c")
 )
+
+testthat::test_that("code_graph returns proper structure of the dependency graph", {
+
+  code <- assert_code(code)
+  pd <- create_pd(code)
+  calls_pd <- extract_calls(pd)
+  graph <- code_graph(calls_pd)
+  testthat::expect_identical(
+    graph,
+    graph_expected
+  )
+
+})
+
+testthat::test_that("graph_parser returns proper code based on code_graph", {
+
+  code <- assert_code(code)
+  pd <- create_pd(code)
+  calls_pd <- extract_calls(pd)
+  graph <- code_graph(calls_pd)
+  names <- 'a'
+  testthat::expect_identical(
+    sapply(names, function(x) graph_parser(x, graph)),
+    graph_expected
+  )
+
+})
+
+
+
 
