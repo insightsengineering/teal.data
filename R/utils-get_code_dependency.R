07a000b4375d746c16ff9f98640f12009d65c882
Marcin <133694481+m7pr@users.noreply.github.com>
2023-12-01 16:09:52 +0100

    bring back old fix_comments
    Signed-off-by: Marcin <133694481+m7pr@users.noreply.github.com>

--- R/utils-get_code_dependency.R
+++ R/utils-get_code_dependency.R
@@ -65,20 +65,15 @@ get_children <- function(pd, parent) {
 
 fix_comments <- function(calls) {
   # If the first token is a COMMENT, then it belongs to the previous call.
-
   if (length(calls) >= 2) {
-    calls_new <- list()
-    calls_new[[1]] <- calls[[1]]
     for (i in 2:length(calls)) {
       if (grepl("@linksto", calls[[i]][1, "text"])) {
-        calls_new[[i - 1]] <- rbind(calls[[i - 1]], calls[[i]][1, ])
+        calls[[i - 1]] <- rbind(calls[[i - 1]], calls[[i]][1, ])
         calls[[i]] <- calls[[i]][-1, ]
-      } else {
-        calls_new[[i]] <- calls[[i]]
-      }
+      } 
     }
   }
-  calls_new
+  calls
 }
 
 # code_graph ----
