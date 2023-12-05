f00cffe76242977e734383f5d1454dd5c607a6a2
Marcin <133694481+m7pr@users.noreply.github.com>
2023-12-05 15:45:52 +0100

    192 Improve `get_code_dependency` (Code Parser) (#201)
    ---------
    
    Signed-off-by: Marcin <133694481+m7pr@users.noreply.github.com>
    Co-authored-by: Dawid Kałędkowski <6959016+gogonzo@users.noreply.github.com>
    Co-authored-by: go_gonzo <dawid.kaledkowski@gmail.com>
    Co-authored-by: Aleksander Chlebowski <114988527+chlebowa@users.noreply.github.com>
    Co-authored-by: Aleksander Chlebowski <aleksander.chlebowski@contractors.roche.com>

---
+++ R/utils-get_code_dependency.R
@@ -0,0 +1,253 @@
+# get_code_dependency ----
+
+#' Get Code Dependency of an Object
+#'
+#' Extract subset of code required to reproduce specific object(s), including code producing side-effects.
+#'
+#' Given a character vector with code, this function will extract the part of the code responsible for creating
+#' the variables specified by `names`.
+#' This includes the final call that creates the variable(s) in question as well as all _parent calls_,
+#' _i.e._ calls that create variables used in the final call and their parents, etc.
+#' Also included are calls that create side-effects like establishing connections.
+#'
+#' It is assumed that object dependency is established by using three assignment operators: `<-`, `=`, and `->` .
+#' Other assignment methods (`assign`, `<<-`) or non-standard-evaluation methods are not supported.
+#'
+#' Side-effects are not detected automatically and must be marked in the code.
+#' Add `# @linksto object` at the end of a line where a side-effect occurs to specify that this line is required
+#' to reproduce a variable called `object`.
+#'
+#' @param code `character` with the code.
+#' @param names `character` vector of object names.
+#'
+#' @return Character vector, a subset of `code`.
+#' Note that subsetting is actually done on the calls `code`, not necessarily on the elements of the vector.
+#'
+#' @keywords internal
+get_code_dependency <- function(code, names) {
+  checkmate::assert_character(code)
+  checkmate::assert_character(names, any.missing = FALSE)
+
+  if (identical(code, character(0)) || identical(trimws(code), "")) {
+    return(code)
+  }
+
+  code <- parse(text = code, keep.source = TRUE)
+  pd <- utils::getParseData(code)
+
+  # Detect if names are actually in code.
+  symbols <- unique(pd[pd$token == "SYMBOL", "text"])
+  if (!all(names %in% symbols)) {
+    warning("Object(s) not found in code: ", toString(setdiff(names, symbols)))
+  }
+
+  calls_pd <- extract_calls(pd)
+
+  graph <- code_graph(calls_pd)
+  ind <- unlist(lapply(names, function(x) graph_parser(x, graph)))
+  as.character(code[unique(ind)])
+}
+
+#' Split the result of `utils::getParseData()` into separate calls
+#'
+#' @param pd (`data.frame`) A result of `utils::getParseData()`.
+#'
+#' @return
+#' A `list` of `data.frame`s.
+#' Each element is a subset of `pd` corresponding to one call in the original code from which `pd` was obtained.
+#' Only four columns (`"token"`, `"text"`, `"id"`, `"parent"`) are kept, the rest is discarded.
+#'
+#' @keywords internal
+#' @noRd
+extract_calls <- function(pd) {
+  calls <- lapply(pd[pd$parent == 0, "id"], get_children, pd = pd)
+  fix_comments(calls)
+}
+
+get_children <- function(pd, parent) {
+  idx_children <- abs(pd$parent) == parent
+  children <- pd[idx_children, c("token", "text", "id", "parent")]
+  if (nrow(children) == 0) {
+    return(NULL)
+  }
+
+  if (parent > 0) {
+    do.call(rbind, c(list(children), lapply(children$id, get_children, pd = pd)))
+  }
+}
+
+fix_comments <- function(calls) {
+  # If the first token is a COMMENT, then it belongs to the previous call.
+  if (length(calls) >= 2) {
+    for (i in 2:length(calls)) {
+      if (grepl("@linksto", calls[[i]][1, "text"])) {
+        calls[[i - 1]] <- rbind(calls[[i - 1]], calls[[i]][1, ])
+        calls[[i]] <- calls[[i]][-1, ]
+      }
+    }
+  }
+  calls
+}
+
+# code_graph ----
+
+#' Create Object Dependencies Graph Within Parsed Code
+#'
+#' @description
+#' Builds dependency graph that identifies dependencies between objects in parsed code.
+#' Helps understand which objects depend on which.
+#'
+#' @param calls_pd `list` of `data.frame`s;
+#'  result of `utils::getParseData()` split into subsets representing individual calls;
+#'  created by `extract_calls()` function
+#'
+#' @return
+#' A list (of length of input `calls_pd`) where each element represents one call.
+#' Each element is a character vector listing names of objects that depend on this call
+#' and names of objects that this call depends on.
+#' Dependencies are listed after the `"<-"` string, e.g. `c("a", "<-", "b", "c")` means that in this call object `a`
+#' depends on objects `b` and `c`.
+#' If a call is tagged with `@linksto a`, then object `a` is understood to depend on that call.
+#'
+#' @keywords internal
+#' @noRd
+code_graph <- function(calls_pd) {
+  cooccurrence <- extract_occurrence(calls_pd)
+
+  side_effects <- extract_side_effects(calls_pd)
+
+  mapply(c, side_effects, cooccurrence, SIMPLIFY = FALSE)
+}
+
+#' Extract Object Occurrence
+#'
+#' @description Extracts objects occurrence within calls passed by `calls_pd`.
+#' Also detects which objects depend on which within a call.
+#'
+#' @param calls_pd `list` of `data.frame`s;
+#'  result of `utils::getParseData()` split into subsets representing individual calls;
+#'  created by `extract_calls()` function
+#'
+#' @return
+#' A list (of length of input `calls_pd`) where each element represents one call.
+#' Each element is a character vector listing names of objects that depend on this call
+#' and names of objects that this call depends on.
+#' Dependencies are listed after the `"<-"` string, e.g. `c("a", "<-", "b", "c")` means that in this call object `a`
+#' depends on objects `b` and `c`.
+#' If a call is tagged with `@linksto a`, then object `a` is understood to depend on that call.
+#'
+#' @keywords internal
+#' @noRd
+extract_occurrence <- function(calls_pd) {
+  is_in_function <- function(x) {
+    # If an object is a function parameter,
+    # then in calls_pd there is a `SYMBOL_FORMALS` entry for that object.
+    function_id <- x[x$token == "FUNCTION", "parent"]
+    if (length(function_id)) {
+      x$id %in% get_children(x, function_id)$id
+    } else {
+      rep(FALSE, nrow(x))
+    }
+  }
+  lapply(
+    calls_pd,
+    function(call_pd) {
+
+      # What occurs in a function body is not tracked.
+      x <- call_pd[!is_in_function(call_pd), ]
+      sym_cond <- which(x$token %in% c("SYMBOL", "SYMBOL_FUNCTION_CALL"))
+
+      if (length(sym_cond) == 0) {return(character(0))}
+      # Watch out for SYMBOLS after $ and @. For x$a x@a: x is object, a is not.
+      # For x$a, a's ID is $'s ID-2 so we need to remove all IDs that have ID = $ID - 2.
+      dollar_ids <- x[x$token %in% c("'$'", "'@'"), "id"]
+      if (length(dollar_ids)) {
+        object_ids <- x[sym_cond, "id"]
+        after_dollar <- object_ids[(object_ids - 2) %in% dollar_ids]
+        sym_cond <- setdiff(sym_cond, which(x$id %in% after_dollar))
+      }
+
+      # If there was an assignment operation detect direction of it.
+      ass_cond <- grep("ASSIGN", x$token)
+      if (length(ass_cond)) { # NOTE(1)
+        sym_cond <- sym_cond[sym_cond > ass_cond] # NOTE(2)
+      }
+      if ((length(ass_cond) && x$text[ass_cond] == "->") || !length(ass_cond)) { # NOTE(3)
+          sym_cond <- rev(sym_cond)
+      }
+      append(unique(x[sym_cond, "text"]), "<-", after = 1)
+
+      ### NOTE(3): What if there are 2+ assignments, e.g. a <- b -> c or e.g. a <- b <- c.
+      ### NOTE(2): For cases like 'eval(expression(b <- b + 2))' removes 'eval(expression('.
+      ### NOTE(1): Cases like 'data(iris)' that do not have an assignment operator.
+      ### NOTE(1): Then they are parsed as c("iris", "<-", "data")
+    }
+  )
+}
+
+#' Extract Side Effects
+#'
+#' @description Extracts all object names from the code that are marked with `@linksto` tag.
+#'
+#' @details
+#' The code may contain functions calls that create side effects, e.g. modify the environment.
+#' Static code analysis may be insufficient to determine which objects are created or modified by such a function call.
+#' The `@linksto` comment tag is introduced to mark a call as having a (side) effect on one or more objects.
+#' With this tag a complete object dependency structure can be established.
+#' Read more about side effects and the usage of `@linksto` tag in [`get_code_dependencies()`] function.
+#'
+#' @param calls_pd `list` of `data.frame`s;
+#'  result of `utils::getParseData()` split into subsets representing individual calls;
+#'  created by `extract_calls()` function
+#'
+#' @return
+#' A list of length equal to that of `calls_pd`, where each element is a character vector of names of objects
+#' depending a call tagged with `@linksto` in a corresponding element of `calls_pd`.
+#'
+#' @keywords internal
+#' @noRd
+extract_side_effects <- function(calls_pd) {
+  lapply(
+    calls_pd,
+    function(x) {
+      linksto <- grep("@linksto", x[x$token == "COMMENT", "text"], value = TRUE)
+      unlist(strsplit(sub("\\s*#\\s*@linksto\\s+", "", linksto), "\\s+"))
+    }
+  )
+}
+
+# graph_parser ----
+
+#' Return the indices of calls needed to reproduce an object
+#'
+#' @param x The name of the object to return code for.
+#' @param graph A result of `code_graph()`.
+#'
+#' @return
+#' Integer vector of indices that can be applied to `graph` to obtain all calls required to reproduce object `x`.
+#'
+#' @keywords internal
+#' @noRd
+graph_parser <- function(x, graph) {
+
+  occurrence <- vapply(graph, function(call) {
+    ind <- match("<-", call, nomatch = length(call) +1L)
+    x %in% call[seq_len(ind - 1L)]
+  },
+  logical(1))
+
+  dependencies <- lapply(graph[occurrence], function(call) {
+    ind <- match("<-", call, nomatch = 0L)
+    call[(ind + 1L):length(call)]
+  })
+  dependencies <- setdiff(unlist(dependencies), x)
+
+  if (length(dependencies) && any(occurrence)) {
+    dependency_ids <- lapply(dependencies, function(dependency) {
+      graph_parser(dependency, graph[1:max(which(occurrence))])
+    })
+    sort(unique(c(which(occurrence), unlist(dependency_ids))))
+  } else {
+    which(occurrence)
+  }
+}
---
+++ tests/testthat/test-get_code.R
@@ -0,0 +1,538 @@
+warning_message <- "warning('Code was not verified for reproducibility.')"
+
+testthat::test_that("get_code with datanames does not brake for empty code", {
+  testthat::expect_identical(
+    get_code(teal_data(a = 1, code = character(0)), datanames = "a"),
+    warning_message
+  )
+  testthat::expect_identical(
+    get_code(teal_data(a = 1, code = ""), datanames = "a"),
+    paste0(warning_message, "\n")
+  )
+})
+
+testthat::test_that("get_code with datanames does not brake for code without symbols", {
+  code <- c(
+    "1 + 1",
+    "a <- 5",
+    "501"
+  )
+
+  testthat::expect_identical(
+    get_code(teal_data(a = 5, code = code), datanames = "a"),
+    paste(warning_message, "a <- 5", sep = "\n")
+  )
+
+})
+
+testthat::test_that("get_code with datanames extracts code of a binding from a simple code put in a character", {
+  code <- c(
+    "a <- 1",
+    "b <- 2"
+  )
+  tdata <- eval_code(teal_data(), code)
+  datanames(tdata) <- c("a", "b")
+  testthat::expect_identical(
+    get_code(tdata, datanames = "a"),
+    "a <- 1"
+  )
+  testthat::expect_identical(
+    get_code(tdata, datanames = "b"),
+    "b <- 2"
+  )
+})
+
+testthat::test_that("get_code works for datanames of length > 1", {
+  code <- c(
+    "a <- 1",
+    "b <- 2"
+  )
+  tdata <- eval_code(teal_data(), code)
+  datanames(tdata) <- c("a", "b")
+  testthat::expect_identical(
+    get_code(tdata, datanames = c("a", "b")),
+    paste("a <- 1", "b <- 2", sep = "\n")
+  )
+})
+
+testthat::test_that("get_code with datanames warns if binding doesn't exist in a code", {
+  code <- c(
+    "a <- 1",
+    "b <- 2"
+  )
+  tdata <- eval_code(teal_data(), code)
+  datanames(tdata) <- c("a", "b")
+  testthat::expect_error(
+    get_code(tdata, datanames = "c"),
+    "Assertion on 'datanames' failed"
+  )
+})
+
+testthat::test_that("get_code with datanames does not fall into a loop", {
+  code <- c(
+    "a <- 1",
+    "b <- a",
+    "c <- b",
+    "a <- c"
+  )
+  tdata <- eval_code(teal_data(), code)
+  datanames(tdata) <- c("a", "b", "c")
+  testthat::expect_identical(
+    get_code(tdata, datanames = "a"),
+    paste(code, collapse = "\n")
+  )
+  testthat::expect_identical(
+    get_code(tdata, datanames = "b"),
+    paste(code[1:2], collapse = "\n")
+  )
+  testthat::expect_identical(
+    get_code(tdata, datanames = "c"),
+    paste(code[1:3], collapse = "\n")
+  )
+})
+
+
+testthat::test_that(
+  "get_code with datanames extracts code of a parent binding but only those evaluated before coocurence",
+  {
+    code <- c(
+      "a <- 1",
+      "b <- a",
+      "a <- 2"
+    )
+    tdata <- eval_code(teal_data(), code)
+    datanames(tdata) <- c("a", "b")
+    testthat::expect_identical(
+      get_code(tdata, datanames = "b"),
+      paste("a <- 1", "b <- a", sep = "\n")
+    )
+  }
+)
+
+testthat::test_that("get_code with datanames extracts code of a parent binding if used as an arg in fun call", {
+  code <- c(
+    "a <- 1",
+    "b <- identity(x = a)",
+    "a <- 2"
+  )
+  tdata <- eval_code(teal_data(), code)
+  datanames(tdata) <- c("a", "b")
+  testthat::expect_identical(
+    get_code(tdata, datanames = "b"),
+    paste("a <- 1", "b <- identity(x = a)", sep = "\n")
+  )
+})
+
+testthat::test_that("get_code with datanames is possible to output the code for multiple objects", {
+  code <- c(
+    "a <- 1",
+    "b <- 2",
+    "c <- 3"
+  )
+  tdata <- eval_code(teal_data(), code)
+  datanames(tdata) <- c("a", "b", "c")
+  testthat::expect_identical(
+    get_code(tdata, datanames = c("a", "b")),
+    paste("a <- 1", "b <- 2", sep = "\n")
+  )
+})
+
+testthat::test_that("get_code with datanames can't extract the code for assign function", {
+
+  testthat::skip("We will tackle 'assign' some day!")
+  code <- c(
+    "a <- 1",
+    "assign('b', 5)",
+    "b <- b + 2"
+  )
+  tdata <- eval_code(teal_data(), code)
+  datanames(tdata) <- c("a", "b")
+  testthat::expect_identical(
+    get_code(tdata, datanames = "b"),
+    paste("assign('b', 5)", "b <- b + 2", sep = "\n")
+  )
+})
+
+testthat::test_that("@linksto tag indicate affected object if object is assigned anywhere in a code", {
+
+  code <- c(
+    "a <- 1",
+    "assign('b', 5) # @linksto b",
+    "b <- b + 2"
+  )
+  tdata <- eval_code(teal_data(), code)
+  datanames(tdata) <- c("a", "b")
+  testthat::expect_identical(
+    get_code(tdata, datanames = "b"),
+    paste("assign(\"b\", 5)", "b <- b + 2", sep = "\n")
+  )
+})
+
+testthat::test_that(
+  "get_code with datanames can extract the code when function creates an object which is used only on rhs",
+  {
+    code <- c(
+      "data(iris)",
+      "iris2 <- head(iris)"
+    )
+    tdata <- eval_code(teal_data(), code)
+    datanames(tdata) <- c("iris2")
+    testthat::expect_identical(
+      get_code(tdata, datanames = "iris2"),
+      paste("data(iris)", "iris2 <- head(iris)", sep = "\n")
+    )
+  }
+)
+
+testthat::test_that("get_code with datanames can extract the code when using <<-", {
+  code <- c(
+    "a <- 1",
+    "b <- a",
+    "b <<- b + 2"
+  )
+  tdata <- eval_code(teal_data(), code)
+  datanames(tdata) <- c("a", "b")
+  testthat::expect_identical(
+    get_code(tdata, datanames = "b"),
+    paste("a <- 1", "b <- a", "b <<- b + 2", sep = "\n")
+  )
+})
+
+testthat::test_that("get_code with datanames detects every assign calls even if not evaluated", {
+  code <- c(
+    "a <- 1",
+    "b <- 2",
+    "eval(expression({b <- b + 2}))"
+  )
+  tdata <- eval_code(teal_data(), code)
+  datanames(tdata) <- c("a", "b")
+  testthat::expect_identical(
+    get_code(tdata, datanames = "b"),
+    paste("b <- 2", "eval(expression({\n    b <- b + 2\n}))", sep = "\n")
+  )
+})
+
+
+# @linksto ---------------------------------------------------------------------------------------------------------
+
+testthat::test_that("@linksto cause to return this line for affected binding", {
+  code <- "
+  a <- 1 # @linksto b
+  b <- 2
+  "
+  tdata <- eval_code(teal_data(), code)
+  datanames(tdata) <- c("a", "b")
+  testthat::expect_identical(
+    get_code(tdata, datanames = "b"),
+    paste("a <- 1", "b <- 2", sep = "\n")
+  )
+})
+
+testthat::test_that(
+  "@linksto returns this line for affected binding
+  even if object is not specificed/created in the same eval_code",
+  {
+    code <- c(
+      "a <- 1 # @linksto b",
+      "b <- 2"
+    )
+    tdata <- eval_code(teal_data(), code)
+    datanames(tdata) <- c("a", "b")
+    testthat::expect_identical(
+      get_code(tdata, datanames = "b"),
+      paste("a <- 1", "b <- 2", sep = "\n")
+    )
+  }
+)
+
+testthat::test_that(
+  "@linksto returns this line for affected binding
+  if object is not specificed in the same element of code",
+  {
+    code <- c(
+      "a <- 1 ",
+      "b <- 2 # @linksto a"
+    )
+    tdata <- eval_code(teal_data(), code)
+    datanames(tdata) <- c("a", "b")
+    testthat::expect_identical(
+      get_code(tdata, datanames = "a"),
+      paste("a <- 1", "b <- 2", sep = "\n")
+    )
+  }
+)
+
+testthat::test_that(
+  "lines affecting parent evaluated after co-occurrence are not included in get_code with datanamesoutput",
+  {
+    code <- c(
+      "a <- 1",
+      "b <- a",
+      "a <- 3"
+    )
+    tdata <- eval_code(teal_data(), code)
+    datanames(tdata) <- c("a", "b")
+    testthat::expect_identical(
+      get_code(tdata, datanames = "b"),
+      paste("a <- 1", "b <- a", sep = "\n")
+    )
+  }
+)
+
+testthat::test_that(
+  "lines affecting parent evaluated after co-occurrence are not included in get_code with datanamesoutput
+  when using @linksto",
+  {
+    code <- c(
+      "a <- 1 ",
+      "b <- 2 # @linksto a",
+      "a <- a + 1",
+      "b <- b + 1"
+    )
+    tdata <- eval_code(teal_data(), code)
+    datanames(tdata) <- c("a", "b")
+    testthat::expect_identical(
+      get_code(tdata, datanames = "a"),
+      paste("a <- 1", "b <- 2", "a <- a + 1", sep = "\n")
+    )
+    testthat::expect_identical(
+      get_code(tdata, datanames = "b"),
+      paste("b <- 2", "b <- b + 1", sep = "\n")
+    )
+  }
+)
+
+testthat::test_that(
+  "@linksto gets extracted if it's a side-effect on a dependent object",
+  {
+    code <- "
+      iris[1:5, ] -> iris2
+      iris_head <- head(iris) # @linksto iris2
+      classes <- lapply(iris2, class)
+    "
+    tdata <- eval_code(teal_data(), code)
+    datanames(tdata) <- c("iris2", "iris_head", "classes")
+    testthat::expect_identical(
+      get_code(tdata, datanames = "classes"),
+      paste("iris2 <- iris[1:5, ]", "iris_head <- head(iris)", "classes <- lapply(iris2, class)", sep = "\n")
+    )
+  }
+)
+
+testthat::test_that(
+  "@linksto gets extracted if it's a side-effect on a dependent object of a dependent object",
+  {
+    code <- "
+      iris[1:5, ] -> iris2
+      iris_head <- head(iris) # @linksto iris3
+      iris3 <- iris_head[1, ] # @linksto iris2
+      classes <- lapply(iris2, class)
+    "
+    tdata <- eval_code(teal_data(), code)
+    datanames(tdata) <- c("iris2", "iris_head", "iris3", "classes")
+    testthat::expect_identical(
+      get_code(tdata, datanames = "classes"),
+      paste("iris2 <- iris[1:5, ]",
+            "iris_head <- head(iris)",
+            "iris3 <- iris_head[1, ]",
+            "classes <- lapply(iris2, class)",
+            sep = "\n")
+    )
+  }
+)
+
+# functions -------------------------------------------------------------------------------------------------------
+
+testthat::test_that("get_code with datanames ignores occurrence in function definition", {
+  code <- c(
+    "b <- 2",
+    "foo <- function(b) { b <- b + 2 }"
+  )
+  tdata <- eval_code(teal_data(), code)
+  datanames(tdata) <- c("b", "foo")
+  testthat::expect_identical(
+    get_code(tdata, datanames = "b"),
+    "b <- 2"
+  )
+  testthat::expect_identical(
+    get_code(tdata, datanames = "foo"),
+    "foo <- function(b) {\n    b <- b + 2\n}"
+  )
+})
+
+testthat::test_that("get_code with datanames ignores occurrence in function definition in lapply", {
+  code <- c(
+    "a <- list(a = 1, b = 2, c = 3)",
+    "b <- lapply(a, FUN = function(x) { x <- x + 1 })",
+    "b <- Filter(function(x) x > 2, b)",
+    "x <- 1",
+    "print(x)"
+  )
+  tdata <- eval_code(teal_data(), code)
+  datanames(tdata) <- c("a", "b", "x")
+  testthat::expect_identical(
+    get_code(tdata, datanames = "x"),
+    paste("x <- 1", "print(x)", sep = "\n")
+  )
+
+})
+
+testthat::test_that("get_code with datanames does not ignore occurrence in function body if object exsits in env", {
+  skip("This is not urgent and can be ommitted with @linksto tag.")
+  code <- c(
+    "a <- list(a = 1, b = 2, c = 3)",
+    "p <- 5", # This is not extracted, even though is used in the next line.
+    "b <- lapply(a, FUN = function(x) { x <- x + p })",
+    "b <- Filter(function(x) x > 2, b)"
+  )
+  tdata <- eval_code(teal_data(), code)
+  datanames(tdata) <- c("a", "p", "b")
+  testthat::expect_identical(
+    get_code(tdata, datanames = "b"),
+    paste(code, sep = "\n")
+  )
+
+})
+
+testthat::test_that("get_code with datanames ignores occurrence in function definition without { curly brackets", {
+  code <- c(
+    "b <- 2",
+    "foo <- function(b) b <- b + 2 "
+  )
+  tdata <- eval_code(teal_data(), code)
+  datanames(tdata) <- c("b", "foo")
+  testthat::expect_identical(
+    get_code(tdata, datanames = "foo"),
+    "foo <- function(b) b <- b + 2"
+  )
+  testthat::expect_identical(
+    get_code(tdata, datanames = "b"),
+    "b <- 2"
+  )
+})
+
+testthat::test_that("get_code with datanames returns custom function calls on object", {
+
+  code <- c(
+    "b <- 2",
+    "foo <- function(b) { b <- b + 2 }",
+    "foo(b)"
+  )
+  tdata <- eval_code(teal_data(), code)
+  datanames(tdata) <- c("b", "foo")
+  testthat::expect_identical(
+    get_code(tdata, datanames = "b"),
+    paste("b <- 2", "foo <- function(b) {\n    b <- b + 2\n}", "foo(b)", sep = "\n")
+  )
+})
+
+testthat::test_that("get_code with datanames detects occurrence of the function object", {
+  code <- c(
+    "a <- 1",
+    "b <- 2",
+    "foo <- function(b) { b <- b + 2 }",
+    "b <- foo(a)"
+  )
+  tdata <- eval_code(teal_data(), code)
+  datanames(tdata) <- c("a", "b", "foo")
+  testthat::expect_identical(
+    get_code(tdata, datanames = "b"),
+    paste("a <- 1", "b <- 2", "foo <- function(b) {\n    b <- b + 2\n}", "b <- foo(a)", sep = "\n")
+  )
+})
+
+testthat::test_that(
+  "Can't detect occurrence of function definition when a formal is named the same as a function",
+  {
+    code <- c(
+      "x <- 1",
+      "foo <- function(foo = 1) 'text'",
+      "a <- foo(x)"
+    )
+    tdata <- eval_code(teal_data(), code)
+    datanames(tdata) <- c("x", "foo", "a")
+    testthat::expect_identical(
+      get_code(tdata, datanames = "a"),
+      paste("x <- 1", "foo <- function(foo = 1) \"text\"", "a <- foo(x)", sep = "\n")
+    )
+  }
+)
+
+# $ ---------------------------------------------------------------------------------------------------------------
+
+testthat::test_that("get_code with datanames understands $ usage and do not treat rhs of $ as objects (only lhs)", {
+  code <- c(
+    "x <- data.frame(a = 1:3)",
+    "a <- data.frame(y = 1:3)",
+    "a$x <- a$y",
+    "a$x <- a$x + 2",
+    "a$x <- x$a"
+  )
+  tdata <- eval_code(teal_data(), code)
+  datanames(tdata) <- c("x", "a")
+  testthat::expect_identical(
+    get_code(tdata, datanames = "x"),
+    "x <- data.frame(a = 1:3)"
+  )
+  testthat::expect_identical(
+    get_code(tdata, datanames = "a"),
+    paste("x <- data.frame(a = 1:3)",
+          "a <- data.frame(y = 1:3)",
+          "a$x <- a$y",
+          "a$x <- a$x + 2",
+          "a$x <- x$a",
+          sep = "\n")
+  )
+})
+
+testthat::test_that("get_code with datanames detects cooccurrence properly even if all objects are on rhs", {
+  code <- c(
+    "a <- 1",
+    "b <- list(c = 2)",
+    "b[[a]] <- 3"
+  )
+  tdata <- eval_code(teal_data(), code)
+  datanames(tdata) <- c("a", "b")
+  testthat::expect_identical(
+    get_code(tdata, datanames = "b"),
+    paste("a <- 1", "b <- list(c = 2)", "b[[a]] <- 3", sep = "\n")
+  )
+})
+
+
+# @ ---------------------------------------------------------------------------------------------------------------
+
+testthat::test_that("get_code with datanames understands @ usage and do not treat rhs of @ as objects (only lhs)", {
+  code <- c(
+    "setClass('aclass', slots = c(a = 'numeric', x = 'numeric', y = 'numeric')) # @linksto a x",
+    "x <- new('aclass', a = 1:3, x = 1:3, y = 1:3)",
+    "a <- new('aclass', a = 1:3, x = 1:3, y = 1:3)",
+    "a@x <- a@y",
+    "a@x <- a@x + 2",
+    "a@x <- x@a"
+  )
+  tdata <- teal_data(x = 1, a = 1, code = code)
+  datanames(tdata) <- c("x", "a")
+  testthat::expect_identical(
+    get_code(tdata, datanames = "x"),
+    paste(
+      warning_message,
+      'setClass("aclass", slots = c(a = "numeric", x = "numeric", y = "numeric"))',
+      'x <- new("aclass", a = 1:3, x = 1:3, y = 1:3)',
+      sep = "\n"
+    )
+  )
+  testthat::expect_identical(
+    get_code(tdata, datanames = "a"),
+    paste(
+      warning_message,
+      'setClass("aclass", slots = c(a = "numeric", x = "numeric", y = "numeric"))',
+      'x <- new("aclass", a = 1:3, x = 1:3, y = 1:3)',
+      'a <- new("aclass", a = 1:3, x = 1:3, y = 1:3)',
+      "a@x <- a@y",
+      "a@x <- a@x + 2",
+      "a@x <- x@a",
+      sep = "\n"
+    )
+  )
+})
