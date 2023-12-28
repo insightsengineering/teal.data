33ffb6f5ed9807e77c0943913d3d8565f0f022dd
unknown <kirar.kartikeya1@gmail.com>
2023-12-28 16:46:17 +0530

    addtional unit-tests.

---
+++ tests/testthat/test-formatters_var_labels.R
@@ -0,0 +1,37 @@
+testthat::test_that("col_labels accepts an empty data.frame", {
+  testthat::expect_error(col_labels(data.frame()), regexp = NA)
+})
+
+testthat::test_that("col_labels' returns empty character vector for a data.frame with no columns", {
+  testthat::expect_equal(col_labels(data.frame()[1:5, ], fill = TRUE), character(0))
+})
+
+testthat::test_that("col_labels' returns a named vector of NA when fill = FALSE and there are no labels", {
+  testthat::expect_equal(
+    col_labels(iris, fill = FALSE),
+    stats::setNames(rep(NA_character_, times = ncol(iris)), nm = colnames(iris))
+  )
+})
+
+testthat::test_that("col_labels' returns a vector of column names when fill = TRUE and there are no labels", {
+  testthat::expect_equal(
+    col_labels(iris, fill = TRUE),
+    stats::setNames(colnames(iris), nm = colnames(iris))
+  )
+})
+
+test_that("col_relabel correctly changes column labels in a data frame", {
+  iris_df <- iris
+  iris_df <- col_relabel(iris_df, Sepal.Length = "Sepal Length of iris flower")
+  testthat::expect_equal(col_labels(iris_df)[["Sepal.Length"]], "Sepal Length of iris flower")
+  testthat::expect_equal(col_labels(iris)[["Sepal.Length"]], NA_character_)
+})
+
+test_that("col_relabel throws an error for non-existent columns", {
+  testthat::expect_error(col_relabel(iris, NonExistentColumn = "Label"))
+})
+
+test_that("col_relabel returns the original data.frame when no new labels are specified", {
+  iris_df <- col_relabel(iris)
+  testthat::expect_equal(iris_df, iris)
+})
