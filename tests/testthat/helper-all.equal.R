079461a58ab114721fd91fc9601d17bfcb83faa6
André Veríssimo <211358+averissimo@users.noreply.github.com>
2024-01-19 11:48:52 +0100

    fix: use testthat API instead of relying on env variable

--- tests/testthat/helper-all.equal.R
+++ tests/testthat/helper-all.equal.R
@@ -1,4 +1,4 @@
 # Register method for testthat::expect_identical to compare join_keys
-if (nzchar(Sys.getenv("TESTTHAT"))) {
+if (testthat::is_testing()) {
   registerS3method("all.equal", "join_keys", all.equal.join_keys)
 }
