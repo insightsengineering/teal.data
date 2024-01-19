319099b9bb26f958c87a2c9a0c1f3817f90a68e5
André Veríssimo <211358+averissimo@users.noreply.github.com>
2024-01-19 15:19:28 +0100

    Registers `all.equal.join_keys` only for testing (#259)
    # Pull Request
    
    <!--- Replace `#nnn` with your issue link for reference. -->
    
    Fixes #256 
    
    #### Alternative solution
    
    Export the method to compare `join_keys` (method ignores internal list
    order, NULL elements and will check attributes)
    
    #### Changes description
    
    - Registers S3 method only during testing. `devtools::run_examples()`
    was sourcing a {testthat} helper file

--- tests/testthat/helper-all.equal.R
+++ tests/testthat/helper-all.equal.R
@@ -1,2 +1,4 @@
-# covr package doesn't detect the .S3method if it is declared within R/ folder
-.S3method("all.equal", "join_keys", all.equal.join_keys)
+# Register method for testthat::expect_identical to compare join_keys
+if (testthat::is_testing()) {
+  registerS3method("all.equal", "join_keys", all.equal.join_keys)
+}
