3ef0707f85b627e74fd4c0624f63fbf1bcdc8ea8
m7pr <marcin.kosinski.mk1@roche.com>
2024-01-24 15:00:06 +0100

    latest change in get_children appends a new row in calls_pd with the call. fix_comments needs to be changed since it assumed first row contains comments that were suprisingly pushed from other call to another one. now fix_comments needs to verify two rows

--- R/utils-get_code_dependency.R
+++ R/utils-get_code_dependency.R
@@ -134,9 +134,14 @@ fix_comments <- function(calls) {
   # If the first token is a COMMENT, then it belongs to the previous call.
   if (length(calls) >= 2) {
     for (i in 2:length(calls)) {
-      if (grepl("@linksto", calls[[i]][1, "text"])) {
+      comments_first <- grepl("@linksto", calls[[i]][1, "text"])
+      comments_second <- grepl("@linksto", calls[[i]][2, "text"]) && calls[[i]][1, "text"] == ""
+      if (comments_first) {
         calls[[i - 1]] <- rbind(calls[[i - 1]], calls[[i]][1, ])
         calls[[i]] <- calls[[i]][-1, ]
+      } else if (comments_second) {
+        calls[[i - 1]] <- rbind(calls[[i - 1]], calls[[i]][1:2, ])
+        calls[[i]] <- calls[[i]][-c(1:2), ]
       }
     }
   }
