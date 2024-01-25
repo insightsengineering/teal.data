740aae7549fe1add8c590003ac03e0775fefb72b
vedhav <vedhaviyash4@gmail.com>
2024-01-25 12:06:59 +0530

    fix: fix broken unit test

--- tests/testthat/test-cdisc_data.R
+++ tests/testthat/test-cdisc_data.R
@@ -1,7 +1,11 @@
 testthat::test_that("cdisc_data returns teal_data object for objects different than old api", {
   adsl_raw <- as.data.frame(as.list(stats::setNames(nm = default_cdisc_join_keys["ADSL", "ADSL"])))
   testthat::expect_s4_class(
-    teal_data(adsl = adsl_raw, check = TRUE),
+    cdisc_data(adsl = adsl_raw, check = FALSE),
+    "teal_data"
+  )
+  testthat::expect_s4_class(
+    cdisc_data(i = iris, code = "i <- iris", check = TRUE),
     "teal_data"
   )
 })
