0573204fafe1856fb0409c9f2663112e05f843fe
go_gonzo <dawid.kaledkowski@gmail.com>
2024-01-25 08:45:08 +0100

    deprecate check

--- tests/testthat/test-cdisc_data.R
+++ tests/testthat/test-cdisc_data.R
@@ -1,7 +1,7 @@
 testthat::test_that("cdisc_data returns teal_data object for objects different than old api", {
   adsl_raw <- as.data.frame(as.list(stats::setNames(nm = default_cdisc_join_keys["ADSL", "ADSL"])))
   testthat::expect_s4_class(
-    teal_data(adsl = adsl_raw, check = TRUE),
+    cdisc_data(ADSL = adsl_raw),
     "teal_data"
   )
 })
