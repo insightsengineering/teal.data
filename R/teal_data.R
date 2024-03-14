8960f5166e4157f495bbf130580af3c2bd6363ea
kartikeya kirar <kirar.kartikeya1@gmail.com>
2024-03-14 17:51:09 +0530

    Update R/teal_data.R
    Co-authored-by: Marcin <133694481+m7pr@users.noreply.github.com>
    Signed-off-by: kartikeya kirar <kirar.kartikeya1@gmail.com>

--- R/teal_data.R
+++ R/teal_data.R
@@ -12,7 +12,7 @@
 #'  object with dataset column relationships used for joining.
 #'  If empty then no joins between pairs of objects.
 #'
-#' @param code (`character`, `language`) optional, code to reproduce the datasets provided in `...`.
+#' @param code (`character`, `language`) optional code to reproduce the datasets provided in `...`.
 #'  Note this code is not executed and the `teal_data` may not be reproducible
 #'
 #' @param check (`logical`) `r lifecycle::badge("deprecated")`
