4ce5c54eac9a97bfbde9513d6eab31ae98b582e9
m7pr <marcin.kosinski.mk1@roche.com>
2024-10-18 12:16:28 +0200

    move if before the asserts

--- R/teal_data-get_code.R
+++ R/teal_data-get_code.R
@@ -108,9 +108,6 @@
 setMethod("get_code",
   signature = "teal_data",
   definition = function(object, deparse = TRUE, names = NULL, datanames = lifecycle::deprecated(), ...) {
-    checkmate::assert_character(names, min.len = 1L, null.ok = TRUE)
-    checkmate::assert_flag(deparse)
-
     if (lifecycle::is_present(datanames)) {
       lifecycle::deprecate_warn(
         when = "0.6.1",
@@ -118,9 +115,12 @@ setMethod("get_code",
         with = "teal.code::get_code(names = )",
         always = TRUE
       )
-      checkmate::assert_character(datanames, min.len = 1L, null.ok = TRUE)
       names <- datanames
     }
+
+    checkmate::assert_character(names, min.len = 1L, null.ok = TRUE)
+    checkmate::assert_flag(deparse)
+
     callNextMethod(object = object, deparse = deparse, names = names, ...)
   }
 )
