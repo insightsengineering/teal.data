39d9f37dc73f6152234ed5b41ae6a7619d0863eb
m7pr <marcin.kosinski.mk1@roche.com>
2024-10-30 11:17:04 +0100

    remove NextMethod comments

--- R/teal_data-extract.R
+++ R/teal_data-extract.R
@@ -25,26 +25,14 @@
 #' @export
 `[.teal_data` <- function(x, names) {
   checkmate::assert_class(names, "character")
-  names_in_env <- intersect(names, ls(get_env(x)))
-  if (!length(names_in_env)) {
+  names <- intersect(names, ls(get_env(x)))
+  if (!length(names)) {
     warning("None of `names` elements exist in `teal_data`. Returning empty `teal_data`.")
     return(teal_data())
   }
-  # From ?NextMethod
-  # To pass `names` to `NextMethod` - it looks like it needs to be called `names`
-  # and created in the environment of the function that calls `NextMehod`.
 
-  # NextMethod works by creating a special call frame for the next method. If no new arguments are supplied, the
-  # arguments will be the same in number, order and name as those to the current method but their values will be
-  # promises to evaluate their name in the current method and environment. Any named arguments matched to ... are
-  # handled specially: they either replace existing arguments of the same name or are appended to the argument list.
-  # They are passed on as the promise that was supplied as an argument to the current environment.
-  # (S does this differently!) If they have been evaluated in the current (or a previous environment) they remain
-  # evaluated. (This is a complex area, and subject to change: see the draft ‘R Language Definition’.)
-
-  names <- names_in_env
-  x <- NextMethod("`[`", x)
-  x@join_keys <- x@join_keys[names_in_env]
+  x <- NextMethod("`[`", x) # takes 'names' from function's environment
+  x@join_keys <- x@join_keys[names]
 
   x
 }
