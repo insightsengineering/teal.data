a0d4890b7816b614ba353be8db2175ccfb6c39ed
m7pr <marcin.kosinski.mk1@roche.com>
2024-11-06 10:17:54 +0100

    update teal.data constructor after changes in the code structure in qenv in teal.code

--- R/teal_data-class.R
+++ R/teal_data-class.R
@@ -17,13 +17,10 @@ setOldClass("join_keys")
 #' @slot env (`environment`) environment containing data sets and possibly auxiliary variables.
 #'  Access variables with [get_var()] or [`[[`].
 #'  No setter provided. Evaluate code to add variables into `@env`.
-#' @slot code (`character`) vector representing code necessary to reproduce the contents of `@env`.
+#' @slot code (`list` of `character`) representing code necessary to reproduce the contents of `@env`.
 #'  Access with [get_code()].
 #'  No setter provided. Evaluate code to append code to the slot.
-#' @slot id (`integer`) random identifier assigned to each element of `@code`. Used internally.
-#' @slot warnings (`character`) vector of warnings raised when evaluating code.
-#'  Access with [get_warnings()].
-#' @slot messages (`character`) vector of messages raised when evaluating code.
+#'  Read more in Code section.
 #' @slot join_keys (`join_keys`) object specifying joining keys for data sets in `@env`.
 #'  Access or modify with [join_keys()].
 #' @slot datanames (`character`) vector of names of data sets in `@env`.
@@ -31,6 +28,15 @@ setOldClass("join_keys")
 #'  Access or modify with [datanames()].
 #' @slot verified (`logical(1)`) flag signifying that code in `@code` has been proven to yield contents of `@env`.
 #'  Used internally. See [`verify()`] for more details.
+#'
+#' @section Code:
+#'
+#' Each code element is a character representing one call. Each element has possible attributes:
+#' - `warnings` (`character`) the warnings output when evaluating the code element
+#' - `messages` (`character`) the messages output when evaluating the code element
+#' - `id (`integer`) random identifier of the code element to make sure uniqueness when joining
+#' - `dependency` (`character`) names of objects that appear in this call and gets affected by this call,
+#' separated by `<-` (objects on LHS of `<-` are affected by this line, and objects on RHS are affecting this line)
 #'
 #' @import teal.code
 #' @keywords internal
@@ -77,10 +83,37 @@ new_teal_data <- function(data,
   }
   verified <- (length(code) == 0L && length(data) == 0L)
 
-  id <- sample.int(.Machine$integer.max, size = length(code))
-
   new_env <- rlang::env_clone(list2env(data), parent = parent.env(.GlobalEnv))
   lockEnvironment(new_env, bindings = TRUE)
+
+  # GO WITH BELOW OR
+  # apply eval_code(code) in here
+  # but eval_code() works on an object and teal_data/qenv does not exist YET
+  attr(code, "id") <- sample.int(.Machine$integer.max, 1)
+  if (length(code)) {
+    split_code <- teal.code:::split_code(code)
+    split_code <- lapply(split_code, function(current_code) {
+      attr(current_code, "id") <- sample.int(.Machine$integer.max, 1)
+      current_call <- parse(text = trimws(current_code), keep.source = TRUE)
+      pd <- utils::getParseData(current_call)
+      pd <- teal.code:::normalize_pd(pd)
+      call_pd <- teal.code:::extract_calls(pd)[[1]]
+
+      attr(current_code, "dependency") <-
+        c(teal.code:::extract_side_effects(call_pd), teal.code:::extract_occurrence(call_pd))
+      current_code
+    })
+  } else {
+    split_code <- list(character(0))
+    # TODO: do we assign
+    # attr(split_code[[1]], "id") <- sample.int() ?
+    # TODO: do we need
+    # attr(split_code[[1]], "dependency") <- cahracter(0) ?
+  }
+
+  # TODO:
+  # warnings are empty and messages are empty
+  # should we reconsider `@verified` field and should we reconsider eval_code in here?
 
   datanames <- .get_sorted_datanames(datanames = datanames, join_keys = join_keys, env = new_env)
 
@@ -88,9 +121,6 @@ new_teal_data <- function(data,
     "teal_data",
     env = new_env,
     code = code,
-    warnings = rep("", length(code)),
-    messages = rep("", length(code)),
-    id = id,
     join_keys = join_keys,
     datanames = datanames,
     verified = verified
