a24bac3c1f4e1132a8eb91be2f690dfd938d0aac
go_gonzo <dawid.kaledkowski@gmail.com>
2024-11-08 18:51:53 +0100

    fix for unverified

--- R/teal_data-extract.R
+++ R/teal_data-extract.R
@@ -24,7 +24,7 @@
 #'
 #' @export
 `[.teal_data` <- function(x, names) {
-  x <- NextMethod("`[`", x, check_names = FALSE) # takes 'names' from function's environment
+  x <- NextMethod("`[`", x, check_code_names = x@verified) # unverified doesn't need warning for code inconsistency
   x@join_keys <- x@join_keys[names]
   x
 }
