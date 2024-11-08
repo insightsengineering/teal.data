159b6dde86d958d9f64678f30d227bf3b9451a26
go_gonzo <dawid.kaledkowski@gmail.com>
2024-11-08 18:09:35 +0100

    fix tests

--- tests/testthat/test-extract.R
+++ tests/testthat/test-extract.R
@@ -1,34 +1,20 @@
-testthat::test_that("`[.` handles empty names", {
-  data <- teal_data(x = 1, a = 2)
-  testthat::expect_warning(
-    testthat::expect_equal(data[character(0)], teal_data())
-  )
-})
-
-testthat::test_that("`[.` fails with names being NA_character_", {
-  data <- teal_data(x = 1, a = 2)
-  testthat::expect_error(data[NA_character_])
-})
-
-testthat::test_that("`[.` throws warning if names is NULL", {
-  data <- teal_data(x = 1, a = 2)
-  testthat::expect_error(
-    data[NULL],
-    "Assertion on 'names' failed: Must be of type 'character', not 'NULL'."
-  )
-})
-
-testthat::test_that("`[.` thorws warnings if names is numeric", {
-  data <- teal_data(x = 1, a = 2)
-  testthat::expect_error(
-    data[1],
-    "Assertion on 'names' failed: Must be of type 'character', not 'double'."
-  )
-})
-
-testthat::test_that("`[.` returns limited join_keys", {
+testthat::test_that("`[.` subsets join_keys also", {
   data <- teal_data(a = 1, b = 2)
-
   join_keys(data) <- join_keys(join_key("a", "b", "x"))
   testthat::expect_length(join_keys(data["a"]), 0)
+})
+
+testthat::test_that("`[.` preserves @verified field", {
+  testthat::expect_false(teal_data(a = 1, b = 2)["a"]@verified)
+  testthat::expect_true(within(teal_data(), a <- 1)["a"]@verified)
+})
+
+testthat::test_that("`[.` warns and subsets if names are present in code", {
+  data <- teal_data(a = 1, b = 2, code = "a <- 1; b <- 2; c <- 3; d <- 4")
+  testthat::expect_warning(
+    subset <- data[c("a", "c", "d")],
+    "Some 'names' do not exist in the environment of the 'teal_data'. Skipping those: c, d."
+  )
+  testthat::expect_identical(subset@code, data@code[c(1, 3, 4)])
+  testthat::expect_identical(as.list(subset), as.list(data)[1])
 })
