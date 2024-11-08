0c5e6772d62150a10c39f919b2155b8c4cdcc780
go_gonzo <dawid.kaledkowski@gmail.com>
2024-11-08 16:33:18 +0100

    fix

--- tests/testthat/test-extract.R
+++ tests/testthat/test-extract.R
@@ -5,18 +5,16 @@ testthat::test_that("`[.` handles empty names", {
   )
 })
 
-testthat::test_that("`[.` handles names as NA_character_", {
+testthat::test_that("`[.` fails with names being NA_character_", {
   data <- teal_data(x = 1, a = 2)
-  testthat::expect_warning(
-    testthat::expect_equal(data[NA_character_], teal_data())
-  )
+  testthat::expect_error(data[NA_character_])
 })
 
 testthat::test_that("`[.` throws warning if names is NULL", {
   data <- teal_data(x = 1, a = 2)
   testthat::expect_error(
     data[NULL],
-    "Assertion on 'names' failed: Must inherit from class 'character', but has class 'NULL'."
+    "Assertion on 'names' failed: Must be of type 'character', not 'NULL'."
   )
 })
 
@@ -24,7 +22,7 @@ testthat::test_that("`[.` thorws warnings if names is numeric", {
   data <- teal_data(x = 1, a = 2)
   testthat::expect_error(
     data[1],
-    "Assertion on 'names' failed: Must inherit from class 'character', but has class 'numeric'."
+    "Assertion on 'names' failed: Must be of type 'character', not 'double'."
   )
 })
 
@@ -32,10 +30,5 @@ testthat::test_that("`[.` returns limited join_keys", {
   data <- teal_data(a = 1, b = 2)
 
   join_keys(data) <- join_keys(join_key("a", "b", "x"))
-  empty_join_keys <- join_keys()
-  attr(empty_join_keys, "names") <- character(0)
-  testthat::expect_equal(
-    join_keys(data["a"]),
-    empty_join_keys
-  )
+  testthat::expect_length(join_keys(data["a"]), 0)
 })
