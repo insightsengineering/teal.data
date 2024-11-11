865962ce83072ac42068c436e8251daa87d0a7d2
Lluís Revilla <lluis.revilla_sancho@roche.com>
2024-11-11 12:17:39 +0100

    Remove get_labels

--- R/deprecated.R
+++ R/deprecated.R
@@ -1,23 +1,3 @@
-.deprecate_function <- function(what, details) {
-  lifecycle::deprecate_stop(
-    when = "0.4.0",
-    what = what,
-    details = details
-  )
-}
-
-#' @rdname col_labels
-#' @include formatters_var_labels.R
-#' @details
-#' `r lifecycle::badge("deprecated")`
-#'
-#' In previous versions of `teal.data` labels were managed with `get_labels()`.
-#' This function is deprecated as of `0.4.0`, use `col_labels` instead.
-#' @export
-get_labels <- function(...) {
-  .deprecate_function("get_labels()", "Use col_labels(data)")
-}
-
 #' Names of data sets in `teal_data` object
 #'
 #' @description
