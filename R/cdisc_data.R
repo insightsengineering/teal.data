ce21695a3812a9a8127a217d117f7893bca31cce
Lluís Revilla <185338939+llrs-roche@users.noreply.github.com>
2024-11-11 14:56:41 +0100

    Remove TealData and check argument (#349)
    # Pull Request
    
    Fixes #344 
    
    The first commit is to deal with TealData and connectors: code and
    documentation removed.
    The second commit is to deal with `check` argument: I've kept the
    documentation of `verify()` all the other related code and documentation
    has been removed.
    
    All these functions already had `deprecate_stop` so they would have
    caused an error on calling them and they have been so for ~1year so I
    think directly removing them is fine.
    Local check (with teal.code 0.5.0.9013 ) doesn't raise warnings, errors
    or notes.
    
    ---------
    
    Signed-off-by: Lluís Revilla <185338939+llrs-roche@users.noreply.github.com>
    Co-authored-by: Marcin <133694481+m7pr@users.noreply.github.com>

--- R/cdisc_data.R
+++ R/cdisc_data.R
@@ -32,7 +32,6 @@
 #'
 cdisc_data <- function(...,
                        join_keys = teal.data::default_cdisc_join_keys[names(rlang::list2(...))],
-                       code = character(0),
-                       check) {
-  teal_data(..., join_keys = join_keys, code = code, check = check)
+                       code = character(0)) {
+  teal_data(..., join_keys = join_keys, code = code)
 }
--- R/teal_data-constructor.R
+++ R/teal_data-constructor.R
@@ -14,7 +14,6 @@
 #' @param code (`character`, `language`) optional code to reproduce the datasets provided in `...`.
 #'  Note this code is not executed and the `teal_data` may not be reproducible
 #'
-#' @param check (`logical`) `r lifecycle::badge("deprecated")`
 #'  Use [verify()] to verify code reproducibility .
 #'
 #' @return A `teal_data` object.
@@ -26,45 +25,19 @@
 #'
 teal_data <- function(...,
                       join_keys = teal.data::join_keys(),
-                      code = character(0),
-                      check) {
+                      code = character(0)) {
   data_objects <- rlang::list2(...)
   if (inherits(join_keys, "join_key_set")) {
     join_keys <- teal.data::join_keys(join_keys)
   }
-  if (!missing(check)) {
-    lifecycle::deprecate_stop(
-      when = "0.4.0",
-      "teal_data(
-        check = 'check argument is deprecated. Use `verify()` to verify code reproducibility.
-        Find more information on https://github.com/insightsengineering/teal/discussions/945'
-      )"
-    )
-  }
 
-  if (
-    checkmate::test_list(
-      data_objects,
-      types = c("TealDataConnector", "TealDataset", "TealDatasetConnector"),
-      min.len = 1
-    )
-  ) {
-    lifecycle::deprecate_stop(
-      when = "0.4.0",
-      "teal_data(
-        data_objects = 'should use data directly. Using TealDatasetConnector and TealDataset is deprecated.
-        Find more information on https://github.com/insightsengineering/teal/discussions/945'
-      )"
-    )
-  } else {
-    if (length(data_objects) > 0 && !checkmate::test_names(names(data_objects), type = "named")) {
-      stop("Dot (`...`) arguments on `teal_data()` must be named.")
-    }
-    methods::new(
-      "teal_data",
-      .xData = data_objects,
-      code = code,
-      join_keys = join_keys
-    )
+  if (length(data_objects) > 0 && !checkmate::test_names(names(data_objects), type = "named")) {
+    stop("Dot (`...`) arguments on `teal_data()` must be named.")
   }
+  methods::new(
+    "teal_data",
+    .xData = data_objects,
+    code = code,
+    join_keys = join_keys
+  )
 }
