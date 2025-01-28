427e186f9f44aa361104e7f902187dc5f6c1665d
Dony Unardi <donyunardi@gmail.com>
2025-01-28 08:46:18 -0800

    update display for get_code and within

--- R/teal_data-constructor.R
+++ R/teal_data-constructor.R
@@ -25,8 +25,8 @@
 #' - It inherits from the environment and methods such as [`$`], [get()], [ls()], [as.list()],
 #' [parent.env()] work out of the box.
 #' - `teal_data` is a locked environment, and data modification is only possible through the
-#' [teal.code::eval_code()] and [teal.code::within.qenv()] functions.
-#' - It stores metadata about the code used to create the data (see [get_code()][get_code,teal_data-method]).
+#' [teal.code::eval_code()] and [`within.qenv()`][teal.code::within.qenv()] functions.
+#' - It stores metadata about the code used to create the data (see [`get_code()`][get_code,teal_data-method]).
 #' - It supports slicing (see [`teal.code::subset-qenv`])
 #' - Is immutable which means that each code evaluation does not modify the original `teal_data`
 #'   environment directly.
@@ -34,7 +34,7 @@
 #'
 #' @return A `teal_data` object.
 #'
-#' @seealso [`teal.code::eval_code`], [get_code,teal_data-method], [join_keys()], [names.teal_data()]
+#' @seealso [`teal.code::eval_code`], [`get_code()`][get_code,teal_data-method], [join_keys()], [names.teal_data()]
 #'
 #' @export
 #'
