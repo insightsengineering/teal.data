24b415518c320ba141d064e4717902d949251098
André Veríssimo <211358+averissimo@users.noreply.github.com>
2025-07-29 13:42:07 +0100

    chore: another reorganization of code for clean docs

--- R/data.R
+++ R/data.R
@@ -1,97 +1,108 @@
-#' Random adverse events analysis dataset
+#' Simulated `CDISC` Data for Examples
 #'
-#' @docType data
-#' @usage rADAE
-#' @inherit random.cdisc.data::cadae format
-#' @keywords datasets
-#' @source [random.cdisc.data::cadae]
-#' @name rADAE
-"rADAE"
-
-#' Random concomitant medications analysis dataset
+#' @description
+#' This package contains random datasets that are used for testing purposes in the `teal.data` package:
 #'
-#' @docType data
-#' @usage rADCM
-#' @inherit random.cdisc.data::cadcm format
-#' @keywords datasets
-#' @source [random.cdisc.data::cadcm]
-#' @name rADCM
-"rADCM"
-
-#' Random response analysis dataset
+#' - `rADSL` Random subject-level analysis dataset
 #'
+#' @name random_cdisc_data
 #' @docType data
-#' @usage rADEX
-#' @inherit random.cdisc.data::cadex format
 #' @keywords datasets
-#' @source [random.cdisc.data::cadex]
-#' @name rADEX
-"rADEX"
-
-#' Random laboratory data analysis dataset
-#'
-#' @docType data
-#' @usage rADLB
-#' @inherit random.cdisc.data::cadlb format
-#' @keywords datasets
-#' @source [random.cdisc.data::cadlb]
-#' @name rADLB
-"rADLB"
-
-#' Random response analysis dataset
-#'
-#' @docType data
-#' @usage rADRS
-#' @inherit random.cdisc.data::cadrs format
-#' @keywords datasets
-#' @source [random.cdisc.data::cadrs]
-#' @name rADRS
-"rADRS"
-
-#' Random subject-level analysis dataset
-#'
-#' @docType data
-#' @usage rADSL
-#' @keywords datasets
-#' @inherit random.cdisc.data::cadsl format
-#' @source [random.cdisc.data::cadsl]
-#' @name rADSL
+#' @source - `rADSL`: [random.cdisc.data::cadsl]
+# nolint start: line_length.
+#' @format - `rADSL`: An object of class `tbl_df` (inherits from `tbl`, `data.frame`) with `r nrow(rADSL)` rows and `r ncol(rADSL)` columns.
+# nolint end: line_length.
+#' @rdname rADSL
 "rADSL"
 
-#' Random tumor response analysis dataset
-#'
+#' @description - `rADAE`: Random adverse events analysis dataset
 #' @docType data
-#' @usage rADTR
 #' @keywords datasets
-#' @inherit random.cdisc.data::cadtr format
-#' @source [random.cdisc.data::cadtr]
-#' @name rADTR
+#' @source - `rADAE`: [random.cdisc.data::cadae]
+# nolint start: line_length.
+#' @format - `rADAE`: An object of class `tbl_df` (inherits from `tbl`, `data.frame`) with `r nrow(rADAE)` rows and `r ncol(rADAE)` columns.
+# nolint end: line_length.
+#' @rdname rADSL
+"rADAE"
+
+#' @description - `rADCM`: Random concomitant medications analysis dataset
+#' @docType data
+#' @keywords datasets
+#' @source - `rADCM`: [random.cdisc.data::cadcm]
+# nolint start: line_length.
+#' @format - `rADCM`: An object of class `tbl_df` (inherits from `tbl`, `data.frame`) with `r nrow(rADCM)` rows and `r ncol(rADCM)` columns.
+# nolint end: line_length.
+#' @rdname rADSL
+"rADCM"
+
+#' @description - `rADEX`: Random response analysis dataset
+#' @docType data
+#' @keywords  datasets
+#' @source - `rADEX`: [random.cdisc.data::cadex]
+# nolint start: line_length.
+#' @format - `rADEX`: An object of class `tbl_df` (inherits from `tbl`, `data.frame`) with `r nrow(rADEX)` rows and `r ncol(rADEX)` columns.
+# nolint end: line_length.
+#' @rdname rADSL
+"rADEX"
+
+#' @description - `rADLB`: Random laboratory data analysis dataset
+#' @docType data
+#' @keywords datasets
+#' @source - `rADLB`: [random.cdisc.data::cadlb]
+# nolint start: line_length.
+#' @format - `rADLB`: An object of class `tbl_df` (inherits from `tbl`, `data.frame`) with `r nrow(rADLB)` rows and `r ncol(rADLB)` columns.
+# nolint end: line_length.
+#' @rdname rADSL
+"rADLB"
+
+#' @description - `rADRS`: Random response analysis dataset
+#' @docType data
+#' @keywords datasets
+#' @source - `rADRS`: [random.cdisc.data::cadrs]
+# nolint start: line_length.
+#' @format - `rADRS`: An object of class `tbl_df` (inherits from `tbl`, `data.frame`) with `r nrow(rADRS)` rows and `r ncol(rADRS)` columns.
+# nolint end: line_length.
+#' @rdname rADSL
+"rADRS"
+
+#' @description - `rADTR`: Random tumor response analysis dataset
+#' @docType data
+#' @keywords datasets
+#' @source - `rADTR`: [random.cdisc.data::cadtr]
+# nolint start: line_length.
+#' @format - `rADTR`: An object of class `tbl_df` (inherits from `tbl`, `data.frame`) with `r nrow(rADTR)` rows and `r ncol(rADTR)` columns.
+# nolint end: line_length.
+#' @rdname rADSL
 "rADTR"
 
-#' Random time to event analysis dataset
-#'
+#' @description - `rADTTE`: Random time to event analysis dataset
 #' @docType data
-#' @usage rADTTE
-#' @inherit random.cdisc.data::cadtte format
 #' @keywords datasets
-#' @source [random.cdisc.data::cadtte]
-#' @name rADTTE
+#' @source - `rADTTE`: [random.cdisc.data::cadtte]
+# nolint start: line_length.
+#' @format - `rADTTE`: An object of class `tbl_df` (inherits from `tbl`, `data.frame`) with `r nrow(rADTTE)` rows and `r ncol(rADTTE)` columns.
+# nolint end: line_length.
+#' @rdname rADSL
 "rADTTE"
 
-#' Random vital signs analysis dataset
-#'
+#' @description - `rADVS`: Random vital signs analysis dataset
 #' @docType data
-#' @usage rADVS
-#' @inherit random.cdisc.data::cadvs format
 #' @keywords datasets
-#' @source [random.cdisc.data::cadvs]
-#' @name rADVS
+#' @source - `rADVS`: [random.cdisc.data::cadvs]
+# nolint start: line_length.
+#' @format - `rADVS`: An object of class `tbl_df` (inherits from `tbl`, `data.frame`) with `r nrow(rADVS)` rows and `r ncol(rADVS)` columns.
+# nolint end: line_length.
+#' @rdname rADSL
 "rADVS"
 
 #' List containing default joining keys for `CDISC` datasets
 #'
-#' @name default_cdisc_join_keys
-#' @usage default_cdisc_join_keys
+#' These represent primary and foreign keys for joining `CDISC` datasets.
+#'
 #' @docType data
 #' @source internal. See `data-raw/cdisc_datasets.yaml` and `data-raw/data.R` for details.
+#' @seealso [join_keys()]
+#' @examples
+#' default_cdisc_join_keys
+#' default_cdisc_join_keys[c("ADSL", "ADAE")] # Subset for ADSL and ADAE
 "default_cdisc_join_keys"
--- R/dummy_function.R
+++ R/dummy_function.R
@@ -3,7 +3,8 @@
 #' Retrieves example CDISC datasets for use in examples and testing.
 #'
 #' This function returns a dummy dataset and should only be used within `teal.data`.
-#' Note that the datasets are not created and maintained in `teal.data`, they are retrieved from [`random.cdisc.data`][random.cdisc.data::random.cdisc.data-package] package.
+#' Note that the datasets are not created and maintained in `teal.data`, they are retrieved from
+#' [`random.cdisc.data`][random.cdisc.data::random.cdisc.data-package] package.
 #'
 #' @param dataname (`character(1)`) name of a CDISC dataset.
 #'
